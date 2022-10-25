a2dfddfd98d6c89f626b2151ab77292d547018eb
nysolntsev <95509550+nysolntsev@users.noreply.github.com>
2022-10-26 01:28:56 +0300

    2610 1st

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -116,7 +116,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images and stuff)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
